f1bb2c7d729716a0b05977b0679ed4bcefe8fe62
nucleaG <nuclea.g@gmail.com>
2025-12-10 15:26:09 +0100

    feat: initial phrases added and useState from phrases added

---
+++ src/data/initialPhrases.js
@@ -0,0 +1,21 @@
+const initialPhrases = [
+    { 
+        id: 1, 
+        phrase: "El coraje no es la ausencia de miedo, sino el triunfo sobre él.", 
+        author: "Nelson Mandela" ,
+        image: 'https://shorturl.at/NfUkm'
+    },
+    { 
+        id: 2, 
+        phrase: "La vida es aquello que te va sucediendo mientras estás ocupado haciendo otros planes.", 
+        author: "John Lennon" ,
+        image: ' https://shorturl.at/s5cjr'
+    },
+    { 
+        id: 3, 
+        phrase: "No mires el reloj, haz lo que hace. Sigue.", 
+        author: "Sam Levenson" ,
+        image: 'https://www.freepik.es/fotos-vectores-gratis/tiempo-metafora'
+    }
+];
+export {initialPhrases};
