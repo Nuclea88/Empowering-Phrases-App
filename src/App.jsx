cdf5b06d688462ca391b2a2aec5d9897ae059a5b
nucleaG <nuclea.g@gmail.com>
2025-12-11 13:55:19 +0100

    fix: check errors and apllied atomic design

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React, { useState } from 'react'
 import './App.css'
+import { phrasesArray } from './data/phrasesArray';
+import { updatePhrase } from './utils/UpdatePhrase';
+import { deletePhrase } from './utils/deletePhrase';
+import { idGenerator } from './utils/idGenerator';
 
 function App() {
   const [phrases, setPhrases] = useState(() => {
     const saved = localStorage.getItem('phrases');
-    return saved ? JSON.parse(saved) : initialPhrases;
+    return saved ? JSON.parse(saved) : phrasesArray;
   });
 
   useEffect(() => {
     localStorage.setItem('phrases', JSON.stringify(phrases));
   }, [phrases]);
 
-  const addPhrase = (newPhrase) => {
+/*  const addPhrase = (newPhrase) => {
     const phrase = {
       id: idGenerator(phrases),
       phrase: newPhrase.phrase,
@@ -27,7 +31,7 @@ function App() {
 
   const deletePhrase = (id) => {
     setPhrases(phrases.filter((p) => p.id !== id));
-  };
+  };*/
 
   return (
     <AppLayout>
