ef5d3fe2f43b402d82ed30b56f47bd62e1a27ff4
nucleaG <nuclea.g@gmail.com>
2025-12-19 14:13:10 +0100

    fix: import link to navbar correctly

--- src/components/organisms/Navbar.jsx
+++ src/components/organisms/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
+
 
 const Navbar = () => {
   return (
@@ -10,6 +11,7 @@ const Navbar = () => {
 
       <div className="flex flex-col md:flex-row gap-2 w-full md:w-auto">
         <Link to="/" className="bg-white text-[#8C5A66] px-4 py-2 rounded text-center font-medium hover:bg-gray-100 transition-colors">
+
           Home
         </Link>
 
